0cbfda47206300441008e3f2a1349ce918de171c
Andre Dolar <p.andredolar@gmail.com>
2024-05-22 01:09:16 +0800

    Added main page options

--- main recreated.go
+++ main recreated.go
@@ -10,13 +10,30 @@ import (
 func main() {
 	fmt.Println("Starting Application")
 	var user string
+	var action int8
 
 	fmt.Print("User: ")
 	fmt.Scan(&user)
-	loadUser(user)
+	user_master_file := loadUser(user)
+
+	fmt.Println(user_master_file)
+
+	for {
+		fmt.Printf("What would you like to do, %v?\n", user)
+		fmt.Println("\t1. Enter Expense")
+		fmt.Println("\t2. View Accounts")
+		fmt.Println("\t3. Quit")
+		fmt.Scan(&action)
+
+		if action == 3 {
+			fmt.Printf("Thank you, %v", user)
+			break
+		}
+	}
+
 }
 
-func loadUser(user string) {
+func loadUser(user string) *excelize.File {
 	file, err := excelize.OpenFile(fmt.Sprintf("./Ledgers/%v_Master.xlsx", user))
 	// file, err := excelize.OpenFile(fmt.Sprintf("Andre_Master.xlsx"))
 	if err != nil {
@@ -25,5 +42,6 @@ func loadUser(user string) {
 
 	account_number, _ := file.GetCellValue("Sheet1", "A2")
 
-	fmt.Printf("loaded user %v, with account number %v", user, account_number)
+	fmt.Printf("loaded user %v, with account number %v\n", user, account_number)
+	return file
 }
