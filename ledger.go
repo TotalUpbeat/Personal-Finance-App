aee5b8c430b52efa1956b817057547835fda1a0d
Andre Dolar <p.andredolar@gmail.com>
2024-05-23 02:23:32 +0800

    made journal entry struct

--- ledger.go
+++ ledger.go
@@ -5,8 +5,16 @@ import (
 )
 
 type ledger struct {
-	ledger_name  string
-	ledger_file  *excelize.File
-	start_amount float64
-	end_amount   float64
+	ledger_name     string
+	ledger_file     *excelize.File
+	start_amount    float64
+	end_amount      float64
+	journal_entries *journal_entry
+}
+
+type journal_entry struct {
+	date        string
+	description string
+	credit      float64
+	debit       float64
 }
