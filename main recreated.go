ea0dbce0c0132cc6f511ff49b9f8f89b88c717f7
Andre Dolar <p.andredolar@gmail.com>
2024-05-24 01:44:11 +0800

    Made reader now global.

--- main recreated.go
+++ main recreated.go
@@ -10,39 +10,41 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
+var reader *bufio.Reader
+
 func main() {
 	fmt.Println("Starting Application")
 	var user string
-	reader := bufio.NewReader(os.Stdin)
+	reader = bufio.NewReader(os.Stdin)
 
-	user, _ = getInput("User: ", reader)
+	user, _ = getInput("User: ")
 	user_master_file := loadUser(user)
 
 	fmt.Println(user_master_file)
 
-	printPrompt(user, reader)
+	menu(user)
 
 }
 
-func getInput(prompt string, reader *bufio.Reader) (string, error) {
+func getInput(prompt string) (string, error) {
 	fmt.Print(prompt)
 	input, err := reader.ReadString('\n')
 
 	return strings.TrimSpace(input), err
 }
 
-func printPrompt(user string, reader *bufio.Reader) {
+func menu(user string) {
 	var action string
 	action, _ = getInput(fmt.Sprintf(
 		"What would you like to do, %v? \n"+
 			"\t1. Enter Expense\n"+
 			"\t2. View Accounts\n"+
-			"\t3. Quit\n", user), reader)
+			"\t3. Quit\n", user))
 	switch action {
 	case "1":
-		printPrompt(user, reader)
+		menu(user)
 	case "2":
-		printPrompt(user, reader)
+		menu(user)
 	case "3":
 		break
 
